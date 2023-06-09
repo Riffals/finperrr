0413c7cde32dec8311460057bb22233d0f6be447
Riffals <99295884+Riffals@users.noreply.github.com>
2023-06-09 21:42:54 +0700

    Update LamanDashboardPelangganController.php

--- app/Http/Controllers/LamanDashboardPelangganController.php
+++ app/Http/Controllers/LamanDashboardPelangganController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-// GPT 
 class LamanDashboardPelangganController extends Controller
 {
     public function index(Request $request)
